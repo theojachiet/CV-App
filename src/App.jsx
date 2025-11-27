4c0ce95c8cd8ca97c3f289ce3bd2b28dc50ddbce
Teoslip <tjachiet@gmail.com>
2025-11-27 23:25:18 +0100

    start educationList component

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,16 @@ export default function App() {
     <>
       <div className="app-container">
         <div className="edit-panel">
-          <CustomInput id="fullName" description="Full Name" value={fullName} onChange={handleChange(setFullName)} />
-          <CustomInput id="email" description="E-mail" value={email} onChange={handleChange(setEmail)} />
-          <CustomInput id="phone" description="Phone Number" value={phoneNumber} onChange={handleChange(setPhoneNumber)} />
-          <CustomInput id="adress" description="Adress" value={adress} onChange={handleChange(setAdress)} />
+          <div className="personal-info">
+            <CustomInput id="fullName" description="Full Name" value={fullName} onChange={handleChange(setFullName)} />
+            <CustomInput id="email" description="E-mail" value={email} onChange={handleChange(setEmail)} />
+            <CustomInput id="phone" description="Phone Number" value={phoneNumber} onChange={handleChange(setPhoneNumber)} />
+            <CustomInput id="adress" description="Adress" value={adress} onChange={handleChange(setAdress)} />
+          </div>
+
+          <div className="education">
+            
+          </div>
         </div>
 
         <div className="cv-container">
@@ -37,17 +43,41 @@ export default function App() {
           </div>
 
           <div className="cv-section">
-
+            <EducationList />
           </div>
         </div>
       </div>
     </>
   )
 }
+
 function CustomInput({ id, description, value, onChange }) {
   return (
     <label htmlFor={id}>{description}
       <input type="text" id={id} value={value} onChange={onChange} />
     </label>
+  )
+}
+
+function EducationList() {
+  const educationItems = edcuations.map(education => {
+    <li key={education.id}>
+      <div className="entry-time-and-place">
+        <p>{education.dates}</p>
+        <p>{education.place}</p>
+      </div>
+
+      <div className="entry-info">
+        <p>{education.school}</p>
+        <p>{education.field}</p>
+      </div>
+    </li>
+  })
+  
+  return (
+    <article>
+      <h2>Education</h2>
+      <ul>{educationItems}</ul>
+    </article>
   )
 }
