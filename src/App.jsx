7fe264cee7ce6f81284e2ebf5559942e45449754
Teoslip <tjachiet@gmail.com>
2025-11-30 11:00:56 +0100

    create cv css file and start styling personal info board. Add svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './CV.css'
 
 export default function App() {
 
@@ -56,14 +57,18 @@ export default function App() {
       <div className="app-container">
         <div className="edit-panel">
           <div className="personal-info">
+            <h3>Personal Info</h3>
             <CustomInput id="fullName" description="Full Name" value={person.fullName} onChange={handlePersonalChange} />
             <CustomInput id="email" description="E-mail" value={person.email} onChange={handlePersonalChange} />
             <CustomInput id="phoneNumber" description="Phone Number" value={person.phoneNumber} onChange={handlePersonalChange} />
-            <CustomInput id="address" description="address" value={person.address} onChange={handlePersonalChange} />
+            <CustomInput id="address" description="Address" value={person.address} onChange={handlePersonalChange} />
           </div>
 
           <div className="education-list">
-            <h3>Education</h3>
+            <div className="education-header">
+              <h3><img src="/src/assets/education.svg" alt="graduation hat" /> Education</h3>
+              <img src="/src/assets/down.svg" alt="down arrow icon" />
+            </div>
             <EducationEditList
               list={educationList}
               setList={setEducationList}
@@ -174,7 +179,7 @@ function EducationEditList({ list, setList, onChange, onAdd, setEditingId, editi
     return (
       <>
         <ul>{displayedList}</ul>
-        <img src='/src/assets/add.svg' onClick={handleAddItem} />
+        <button onClick={handleAddItem}><img src='/src/assets/add.svg' alt='Plus icon' /> Education</button>
       </>
     )
   }
@@ -194,18 +199,20 @@ function EducationItemEdit({ item, onEdit, onDelete, id, editingId, setEditingId
           <CustomInput id="location" description="Location" value={item.location} onChange={e => onEdit(id, 'location', e.target.value)} />
         </div>
         <button onClick={() => setEditingId(null)}>Save <img src="/src/assets/save.svg" alt="save icon" /></button >
-        <img src='/src/assets/delete.svg' alt='delete icon' onClick={() => {
+        <button onClick={() => {
           onDelete(item.id);
           setEditingId(null);
-        }} />
+        }}>
+          <img src='/src/assets/delete.svg' alt='delete icon' />
+        </button>
       </>
     )
   } else {
     itemContent = (
       <>
         {item.school}
-        <img src='/src/assets/edit.svg' alt='edit icon' onClick={() => setEditingId(item.id)} />
-        <img src='/src/assets/delete.svg' alt='delete icon' onClick={() => onDelete(item.id)} />
+        <button onClick={() => setEditingId(item.id)}><img src='/src/assets/edit.svg' alt='edit icon' /></button>
+        <button onClick={() => onDelete(item.id)}><img src='/src/assets/delete.svg' alt='delete icon' /></button>
       </>
     )
   }
